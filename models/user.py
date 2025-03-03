bd8ee0119b2203a4a501d7f29987f9e00e31fdcd
Kale-Francis <kalefrancis989@gmail.com>
2025-03-03 01:57:29 +0000

    files updated

--- models/user.py
+++ models/user.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from models.base_model import BaseModel
 
 class User(BaseModel):
